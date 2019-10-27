4a4b50d4e3b32e6137e9ecd14a3c43426ab99478
Tiago <t.marques999@gmail.com>
2019-10-27 22:16:55 +0000

    Adds input

---
+++ src/components/Input.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <span>Add something new:</span>
+    <input type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Input"
+};
+</script>
+
+<style scoped>
+.container {
+  align-self: stretch;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 2rem 0;
+}
+
+span {
+  font-size: 2rem;
+}
+
+input {
+  margin: 1rem 0;
+  width: 40%;
+  height: 2.5rem;
+  text-align: center;
+  font-size: 2rem;
+  border-radius: 10px;
+  border: 1px solid turquoise;
+}
+
+input:focus {
+  outline: none;
+  border-radius: 10px;
+  border: 2px solid skyblue;
+}
+
+@media only screen and (min-width: 768px) {
+  input {
+    margin: 2rem;
+  }
+}
+</style>
