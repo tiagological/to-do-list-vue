e1d3715f132b348a09bb6336f6b1cba231b32c92
Tiago <t.marques999@gmail.com>
2019-10-27 22:16:30 +0000

    Adds VueLogo

---
+++ src/components/VueLogo.vue
@@ -0,0 +1,24 @@
+<template>
+  <img src="../assets/logo.png" alt="Vue logo" />
+</template>
+
+<script>
+export default {
+  name: "VueLogo"
+};
+</script>
+
+<style scoped>
+img {
+  height: 5rem;
+  width: auto;
+  margin: 2rem 0;
+}
+
+@media only screen and (min-width: 1024px) {
+  img {
+    height: 7.5rem;
+    width: auto;
+  }
+}
+</style>
