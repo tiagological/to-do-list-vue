f075d69102e97350168cc0e81a99635a35d73859
Tiago <t.marques999@gmail.com>
2019-10-27 22:16:42 +0000

    Adds title

---
+++ src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="title">{{ title }}</div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style scoped>
+.title {
+  color: #35495e;
+}
+
+@media only screen and (min-width: 1024px) {
+  .title {
+    font-size: 3rem;
+  }
+}
+</style>
