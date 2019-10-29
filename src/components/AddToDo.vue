7e25b2c08e793b5d4daaaaa8dc3776eb7f5e8c93
Tiago <t.marques999@gmail.com>
2019-10-29 00:21:44 +0000

    Renames and refactors input --> addToDo

---
+++ src/components/AddToDo.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="container">
+    <span>Add something new:</span>
+    <form v-on:submit="addToDo">
+      <input
+        placeholder="Type something..."
+        type="text"
+        name="todo"
+        v-model="text"
+        class="input-text-field"
+      />
+      <button type="submit" class="add-btn">
+        <font-awesome-icon icon="plus" class="add-icon" />
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import uuid from "uuid";
+export default {
+  name: "AddToDo",
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+    addToDo(e) {
+      e.preventDefault();
+      if (this.text.length === 0) {
+        return;
+      }
+
+      const newToDo = {
+        id: uuid.v4(),
+        text: this.text,
+        completed: false
+      };
+
+      this.$emit("add-todo", newToDo);
+      this.text = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  align-self: stretch;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 2rem 0;
+}
+
+span {
+  font-size: 1.5rem;
+  color: #42b983;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.input-text-field {
+  margin: 1rem 0;
+  width: 100%;
+  height: 4rem;
+  padding: 1rem;
+  text-align: center;
+  font-size: 1.25rem;
+  border-radius: 5px;
+  border: 1px solid #35495e;
+  color: gray;
+}
+
+.input-text-field:focus {
+  outline: none;
+  border-radius: 5px;
+  border: 2px solid skyblue;
+}
+
+.add-btn {
+  font-weight: bold;
+  border-style: none;
+  border-radius: 50%;
+  background-color: transparent;
+  color: #008000;
+  height: 3rem;
+  width: 3rem;
+  transition: all 200ms;
+}
+
+.add-btn:hover {
+  cursor: pointer;
+  background-color: #c7ffc7;
+}
+
+.add-icon {
+  height: 2rem;
+  width: auto;
+}
+
+@media only screen and (min-width: 768px) {
+  .input-text-field {
+    margin: 2rem;
+    width: 40%;
+  }
+}
+</style>
