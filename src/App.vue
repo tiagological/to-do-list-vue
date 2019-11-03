33a778f84416cf0c757796aaccf80ccedcc9cc09
Tiago <t.marques999@gmail.com>
2019-11-03 20:54:47 +0000

    Changes colour scheme and adds animation to placeholder
    Also removes Vue logo and edits title prop

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <VueLogo />
-    <Title title="Vue To Do List" />
-    <AddToDo v-on:add-todo="addToDo" />
+    <header>
+      <Title title="To Do List" />
+      <AddToDo v-on:add-todo="addToDo" />
+    </header>
     <List :list="list" v-on:delete-todo="deleteTodo" />
-    <h1 v-if="list.length === 0">Nothing to do!</h1>
+    <transition name="fade">
+      <h2 v-if="list.length === 0" class="placeholder">Add something to your list</h2>
+    </transition>
   </div>
 </template>
 
 
 <script>
-import VueLogo from "./components/VueLogo";
 import Title from "./components/Title";
 import AddToDo from "./components/AddToDo";
 import List from "./components/List";
@@ -18,7 +20,6 @@ import List from "./components/List";
 export default {
   name: "app",
   components: {
-    VueLogo,
     Title,
     AddToDo,
     List
@@ -28,22 +29,31 @@ export default {
       list: []
     };
   },
+  created() {
+    const storedList = JSON.parse(localStorage.getItem("list"));
+    this.list = storedList || [];
+  },
   methods: {
     deleteTodo: function(id) {
       this.list = this.list.filter(item => item.id !== id);
+      localStorage.setItem("list", JSON.stringify(this.list));
     },
     addToDo(newToDo) {
       this.list = [...this.list, newToDo];
+      localStorage.setItem("list", JSON.stringify(this.list));
     }
   }
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Muli&display=swap");
+
 html,
 body {
   height: 100%;
   width: 100%;
+  font-family: "Muli", sans-serif;
 }
 
 * {
@@ -53,19 +63,46 @@ body {
 }
 
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Muli", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   height: 100%;
   width: 100%;
-  padding: 1rem;
+  background-color: #2f2fa2;
+  overflow-y: auto;
+  overflow-x: hidden;
   display: flex;
   flex-direction: column;
   align-items: center;
   box-sizing: inherit;
 }
 
-h1 {
-  color: #35495e;
+header {
+  background-color: #f64c72;
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 75%, 50% 100%, 0% 75%);
+  margin-bottom: 2rem;
+}
+
+.placeholder {
+  color: #fff;
+}
+
+.fade-enter {
+  opacity: 0;
+  transform: translateY(100px);
+}
+
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition-delay: 1s;
+  transition-property: opacity, transform;
+}
+
+.fade-leave-active {
+  transition: opacity 200ms;
 }
 </style>
