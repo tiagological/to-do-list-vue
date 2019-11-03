3a16fd60191caf0ce89ba5195128d26c64ac798f
Tiago <t.marques999@gmail.com>
2019-11-03 20:59:45 +0000

    Restyles item component

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
-    <p>{{item.text}}</p>
-    <button class="del-btn" v-on:click="$emit('delete-todo', item.id)">
-      <font-awesome-icon icon="minus" class="del-icon" />
-    </button>
+  <div class="item-container">
+    <div class="inner-container">
+      <span>{{item.text}}</span>
+
+      <button class="del-btn" v-on:click="$emit('delete-todo', item.id)">
+        <font-awesome-icon icon="minus" class="del-icon" />
+      </button>
+    </div>
   </div>
 </template>
 
@@ -17,19 +20,32 @@ export default {
 </script>
 
 <style scoped>
-div {
-  margin: 1rem 0;
+.item-container {
+  border-top: 1px solid rgb(0, 0, 0, 0.3);
   border-radius: 5px;
+  margin-bottom: 1rem;
+  background-color: #fff;
+  width: 100%;
+}
+
+@media only screen and (min-width: 1024px) {
+  .item-container {
+    width: 90%;
+  }
+}
+
+.inner-container {
+  width: 100%;
   padding: 0.5rem 1rem;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-between;
-  box-shadow: 0 0 3px #35495e;
 }
 
-p {
+span {
   font-size: 1.25rem;
+  word-break: break-all;
+  hyphens: auto;
 }
 
 .del-btn {
@@ -37,10 +53,11 @@ p {
   font-weight: bold;
   height: 3rem;
   width: 3rem;
+  margin-left: auto;
+  flex-shrink: 0;
   background-color: transparent;
   border: transparent;
   border-radius: 50%;
-  transition: all 200ms;
 }
 
 .del-btn:hover {
