d0e9fdd4c618bce7c95ed82ff2f2ebc431b21be8
Tiago <t.marques999@gmail.com>
2019-11-10 13:32:40 +0000

    Creates Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <a href="https://www.tsmarques.com">
+      <span>By TSMarques</span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+  
+<style scoped>
+.container {
+  width: 100%;
+  margin-top: auto;
+  margin-bottom: 2rem;
+  display: flex;
+  justify-content: center;
+  font-size: 1.25rem;
+}
+
+a {
+  text-decoration: none;
+}
+
+a:hover {
+  cursor: pointer;
+}
+
+span {
+  color: #fff;
+}
+</style>
